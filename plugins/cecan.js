3226a040c19563e397212d96f34a4947c9f02392
©ʍɾ ℂ𝕪𝕓𝕖𝕣 ʍɑղմӀ օƒƒíϲíɑӀ♠️ <157870475+manulofficial7@users.noreply.github.com>
2024-07-01 16:17:30 +0530

    Delete plugins directory

--- plugins/cecan.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-	conn.sendFile(m.chat, 'https://api.zahwazein.xyz/randomasupan/cecan?apikey=zenzkey_f07b21f698', 'cecan.mp4', 'Nih kak', m)
-}
-handler.help = ['cecan']
-handler.tags = ['internet']
-
-handler.command = /^(cecan)$/i
-export default handler 
--- plugins/cogan.js
+++
@@ -1,8 +0,0 @@
-let handler = async (m, { conn }) => {
-	conn.sendFile(m.chat, 'https://api.zahwazein.xyz/randomimage/cogan?apikey=zenzkey_f07b21f698', 'cogan.mp4', 'Nih kak', m)
-}
-handler.help = ['cogan']
-handler.tags = ['internet']
-
-handler.command = /^(cogan)$/i
-export default handler 
